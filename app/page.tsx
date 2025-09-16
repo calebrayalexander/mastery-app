fb57ad98e01a5de6b8f1300aae48ff11d99c64ed
Get Trolled <calebrayalexander@gmail.com>
2025-09-16 11:09:24 -0500

    update homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{ display: "flex", height: "100vh", justifyContent: "center", alignItems: "center" }}>
-      <h1 style={{ fontSize: "3rem", color: "red" }}>
-        🚀 HELLO WORLD — it’s working! 🚀
+      <h1 style={{ fontSize: "3rem", color: "blue" }}>
+        🗳️ Caleb for President 2044 🗳️
       </h1>
     </main>
   );
